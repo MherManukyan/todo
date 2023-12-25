10a5acdcb746a93d580c2bb9870374d4a55a6f70
Manukyan <mher.manukyan.2019@gmail.com>
2023-12-25 22:42:32 +0400

    second commit

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
 
 	return (
 		<div className='text-white w-4/5 mx-auto'>
-			<h1 className='text-2xl font-bold text-center mb-10'>Todo for junior</h1>
+			<h1 className='text-2xl font-bold text-center mb-10'>Simple Todo</h1>
 			{todos.map(todo => (
 				<TodoItem
 					key={todo._id}
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import Home from './components/screens/Home/Home'
 import Layout from './components/layout/Layout'
+import Home from './components/screens/Home/Home'
+import './index.css'
 
 ReactDOM.render(
 	<React.StrictMode>
